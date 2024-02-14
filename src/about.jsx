d2601a231a0de14668b03558b34330d7ba401ebe
Nikhil <xnick126@gmail.com>
2024-02-14 22:42:31 +0530

    made it more responsive

--- src/about.jsx
+++ src/about.jsx
@@ -5,7 +5,7 @@ function About() {
             <div className="subdiv-aboutme">
                 <div className="subdiv-aboutme-sub">
                     <p>Hello, I'm <span>Nikhil Mukundan </span>, an enthusiastic student pursuing Computer Science Engineering, specializing in
-                        Front-End Development.<br /><br />My expertise lies in crafting engaging
+                    <span> Front-End Development.</span><br /><br />My expertise lies in crafting engaging
                         user experiences and transforming concepts into tangible outcomes through technologies such as React
                         and Tailwind CSS. I actively seek opportunities to learn and embrace new challenges, and am
                         eager to contribute to inventive projects while collaborating with fellow tech enthusiasts.<br /><br />
